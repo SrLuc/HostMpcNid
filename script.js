8dcb88f05fcb23770c92b6f61cd0b14d8a16bacc
Claudio128 <cllaudio128@gmail.com>
2023-01-10 20:13:32 -0300

    att

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function alertFormPSI() {
     console.log(alert('Seu Email foi enviado com Sucesso! Aguarde o Retorno em seu endereço de Email'));
 }
+
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e){
+    
+    const name = document.querySelector("#inputUnit");
+    
+    const value = name.value;
+    
+    console.log(value);
+
+    if (value == "Coelhos"){
+        document.getElementById('form').action = 'https://formsubmit.co/cllaudio128@gmail.com';
+    }
+    if (value == "Piedade"){
+        document.getElementById('form').action = 'https://formsubmit.co/claudioandrade.nid@gmail.com';
+    }
+    if (value == "Recife"){
+        document.getElementById('form').action = 'https://formsubmit.co/ruanlucenaramos@gmail.com';
+    }
+})
